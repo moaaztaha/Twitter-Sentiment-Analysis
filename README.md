fb187368d07e8d3de39aeb2c1d8392d613a0a53d
moaaztaha <moaaztaha7@gmail.com>
2018-07-02 18:34:17 +0200

    Adding README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Analysing Twitter Tweets using [TextBlob](http://docs.tweepy.org/en/v3.5.0/index.html) and [Tweepy](http://www.tweepy.org/)
+### Based on tutorial by [Siraj](https://www.youtube.com/channel/UCWN3xxRkmTPmbKwht9FuE5A)
+
+## To-do
+### Get Tweets From Twitter Timeline using Tweepy
+### Analyse Tweets using TextBlob 
+### Print Precentage of Polarity and Subjectivity 
--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -2,12 +2,12 @@ print("Collecting Data....")
 from textblob import TextBlob 
 import tweepy 
 
+#Authintication
 consumer_key = 'Consumer Key'
 consumer_secret = 'Consumer Secret'
 
 access_token = 'Access Token'
 access_token_secret = 'Access Token Secret'
-
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -15,13 +15,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
+#Getting The 100 Most Recent Tweets 
 tweets  = api.user_timeline("sirajraval", count = 100)
 
+
+#Counting Polarity and Subjectivity
 polarity = 0
 subjectivity = 0
-
-
-
 for tweet in tweets:
 	analysis = TextBlob(tweet.text)
 	polarity += analysis.sentiment.polarity
